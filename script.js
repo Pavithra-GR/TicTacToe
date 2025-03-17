cd6edc41d4f84d24711a8f43faa24845e2fe6381
PavithraGR <pavithrashakthevel@gmail.com>
2025-03-17 10:19:05 +0530

    Second commit

--- script.js
+++ script.js
@@ -23,13 +23,25 @@
 let turno = true; // true = X's turn, false = O's turn
 const boxes = document.querySelectorAll(".box");
 const btn = document.querySelector("#resetbtn");
+const turnText = document.querySelector("p");
+
 
 const winPatterns = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8], 
     [0, 3, 6], [1, 4, 7], [2, 5, 8], 
     [0, 4, 8], [2, 4, 6]             
 ];
-
+function updateTurnIndicator() {
+    if (turno) {
+        turnText.innerText = "X's Turn";
+        turnText.classList.remove("o-turn");
+        turnText.classList.add("x-turn");
+    } else {
+        turnText.innerText = "O's Turn";
+        turnText.classList.remove("x-turn");
+        turnText.classList.add("o-turn");
+    }
+}
 function checkWinner() {
     winPatterns.forEach(pat=>{
         let [a,b,c]=pat;
@@ -38,9 +50,11 @@ function checkWinner() {
         let boxC=boxes[c].innerText;
         if(boxA && boxA ===boxB && boxA===boxC){
             alert(`${boxA} wins!`);
-            
+            boxes.forEach(box => box.removeEventListener("click", handleClick));
         }
+    
     })
+    updateTurnIndicator();
 }
 
 function handleClick(event) {
@@ -52,8 +66,12 @@ function handleClick(event) {
 }
 
 function resetBoard() {
-    boxes.forEach(box => box.innerText = "");
+    boxes.forEach(box => {
+        box.innerText = "";
+        box.addEventListener("click", handleClick); 
+});
     turno = true;
+    updateTurnIndicator();
 }
 
 // Attach event listeners
